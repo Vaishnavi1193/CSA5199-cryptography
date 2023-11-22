bfe37c13b1eaff42fa761e3ff0d2b79445a93ef8
Vaishnavi1193 <107098851+Vaishnavi1193@users.noreply.github.com>
2023-11-22 12:28:48 +0530

    Update 11.Playfair cipher.c

--- 11.Playfair cipher.c
+++ 11.Playfair cipher.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+
 unsigned long long factorial(int n) {
- if (n <= 1)
- return 1;
- return n * factorial(n - 1);
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
 int main() {
- int keyLength = 25;
- unsigned long long totalPossibleKeys = factorial(keyLength);
- printf("Total possible keys (without considering identical encryption results): %llu\n",
-totalPossibleKeys);
- unsigned long long effectivelyUniqueKeys = totalPossibleKeys / keyLength;
- printf("Effectively unique keys (considering identical encryption results): %llu\n",
-effectivelyUniqueKeys);
- return 0;
-}
+    int n = 25;  
+    unsigned long long totalKeys = factorial(n);
+    printf("Total number of possible keys: %llu\n", totalKeys);
 
+    return 0;
+}
 OUTPUT:
-Total possible keys (without considering identical encryption results): 7034535277573963776
-Effectively unique keys (considering identical encryption results): 281381411102958551
+Total number of possible keys: 7034535277573963776
